4501bd5ce96ca108ab5663121dd922aeb165d381
IYKYKY <151485062+IYKYKY@users.noreply.github.com>
2024-07-23 10:26:41 +0200

    Update buyers.model.js

--- backend/models/buyers.model.js
+++ backend/models/buyers.model.js
@@ -1,1 +1,34 @@
+const mongoose = require('mongoose');
 
+const buyerSchema = new mongoose.Schema({
+  areaOfInterest: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    },
+  phone: {
+    type: String,
+    required: true
+  },
+  typeOfFinancing: {
+    type: String,
+    required: true
+  },
+  timelineToClose: {
+    type: String,
+    required: true
+  },
+  budget: {
+    type: String,
+    required: true
+  },
+});
+
+module.exports = mongoose.model('Buyer', buyerSchema);
